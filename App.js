ed962984d2c4c930b800aa35efc34eaede0ad08d
Aryansingh9285 <as.aryansingh2004@gmail.com>
2024-12-25 21:24:02 +0530

    Episode-1

---
+++ App.js
@@ -0,0 +1,12 @@
+const header =React.createElement("h1",{id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h3",{},"Name: Aryan Singh"),
+        React.createElement("h4",{},"Roll no: BETN1cs21039")
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h3",{},"Name: Aryan Singh"),
+        React.createElement("h4",{},"Roll no: BETN1cs21039")
+    ]),
+])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(header)
